6b1af6d2e359d42720d56e00ae76ec90086f27ee
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-08-02 23:27:26 +0200

    feat: add routes
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { GlobalStyles } from "./global.styles";
-import Pages from "./Pages/Home";
+import Category from "./Components/Category/Category";
+import Pages from "./Pages/Pages";
 
 export const App = () => (
-  <>
+  <BrowserRouter>
+    <Category />
     <Pages />
     <GlobalStyles />
-  </>
+  </BrowserRouter>
 );
--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Cuisine from "./Cuisine";
 import Home from "./Home";
+import NoMatch from "./NoMatch";
 
 function Pages() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+    </Routes>
   );
 }
 
