70db96cf45ff4ec93372f96b9b9415b1f73b8c02
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-08-02 23:32:14 +0200

    feat: add not found page
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Home from "./Home";
-import NoMatch from "./NoMatch";
+import PageNotFound from "./PageNotFound";
 
 function Pages() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
