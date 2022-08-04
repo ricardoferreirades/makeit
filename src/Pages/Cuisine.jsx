fa526025098404c11147d730776aef9dd166744c
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-08-04 22:11:38 +0200

    feat: feach cuisines by type
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/Pages/Cuisine.jsx
+++ src/Pages/Cuisine.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Cuisine() {
-  return <div>Cuisine</div>;
+  const [cuisine, setCuisine] = useState([]);
+  const params = useParams();
+  const getCuisine = async (name) => {
+    try {
+      const data = await fetch(
+        `${process.env.BASE_URL}/complexSearch?apiKey=${process.env.API_KEY}&cuisine=${name}&number=9&`
+      );
+      const receips = await data.json();
+      setCuisine(receips.results);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    console.log(params);
+    getCuisine(params.type);
+  }, [params.type]);
+  return (
+    <>
+      {cuisine.map((_cuisine) => (
+        <div key={_cuisine.id}>{_cuisine.title}</div>
+      ))}
+    </>
+  );
 }
 
 export default Cuisine;
